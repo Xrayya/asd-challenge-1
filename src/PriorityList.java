5c46c8425e0101afb51f00d0c8ecdfd101addd1a
Xrayya <nur.alkhiyar@gmail.com>
2022-11-16 22:29:50 +0700

    build up PriorityQueue

--- src/PriorityList.java
+++ src/PriorityList.java
@@ -1,4 +1,4 @@
-public class PriorityList<T> implements IPriorityQueue<T> {
+public class PriorityList<T extends Comparable<T>> implements IPriorityQueue<T> {
     NodeArray<T> head;
     NodeArray<T> tail;
 
@@ -24,14 +24,16 @@ public class PriorityList<T> implements IPriorityQueue<T> {
 
     @Override
     public void listAllElements() {
-        // TODO: Auto-generated method stub
-
+        NodeArray<T> node = this.head;
+        while (node != null) {
+            System.out.printf("index %d : %s\n", node.index, node.data);
+            node = (NodeArray<T>) node.next;
+        }
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO: Auto-generated method stub
-        return false;
+        return this.head == null;
     }
 
     @Override
@@ -39,50 +41,122 @@ public class PriorityList<T> implements IPriorityQueue<T> {
         NodeArray<T> node = new NodeArray<T>(data);
         node.index = tail.index + 1;
 
-        // TODO: Heapify!
+        tail.next = node;
+        node.prev = tail;
+        tail = node;
 
-        return false;
+        this.heapify(tail.index);
+
+        return true;
     }
 
     @Override
     public NodeArray<T> dequeue() {
-        // TODO: Auto-generated method stub
-        return null;
+        this.swap(this.head, this.tail);
+
+        NodeArray<T> node = this.tail;
+        this.tail = (NodeArray<T>) node.prev;
+        this.tail.next = null;
+        node.prev = null;
+
+        this.heapify(this.head.index);
+
+        return node;
+    }
+
+    public NodeArray<T> removeAt(int index) {
+        this.swap(this.getItemAt(index), this.tail);
+
+        NodeArray<T> node = this.tail;
+        this.tail = (NodeArray<T>) node.prev;
+        this.tail.next = null;
+        node.prev = null;
+
+        this.heapify(index);
+
+        return node;
     }
 
     @Override
     public NodeArray<T> peek() {
-        // TODO: Auto-generated method stub
-        return null;
+        return this.head;
     }
 
     @Override
     public NodeArray<T> getItemAt(int index) {
-        // TODO: Auto-generated method stub
+        // if (index > tail.index) {
+        // return null;
+        // }
+
+        NodeArray<T> tmp = this.head;
+
+        while (tmp.next != null) {
+            if (tmp.index == index) {
+                return tmp;
+            }
+            tmp = (NodeArray<T>) tmp.next;
+        }
+
         return null;
     }
 
     @Override
     public NodeArray<T> getParentOf(int index) {
-        // TODO: Auto-generated method stub
-        return null;
+        return getItemAt((index - 1) / 2);
     }
 
     @Override
     public NodeArray<T> getLeftChildOf(int index) {
-        // TODO: Auto-generated method stub
-        return null;
+        return getItemAt(2 * index + 1);
     }
 
     @Override
     public NodeArray<T> getRightChildOf(int index) {
-        // TODO: Auto-generated method stub
-        return null;
+        return getItemAt(2 * index + 2);
+
     }
 
     @Override
     public void heapify(int index) {
-        // TODO: Auto-generated method stub
+        if (index == tail.index) {
+            this.shiftUp(index);
+            return;
+        }
 
+        shiftDown(index);
+    }
+
+    private void shiftUp(int index) {
+        NodeArray<T> currentNode = this.getItemAt(index);
+        NodeArray<T> parentNode = this.getParentOf(index);
+
+        if (currentNode.data.compareTo(parentNode.data) < 0) {
+            swap(currentNode, parentNode);
+        }
+
+        this.shiftUp(parentNode.index);
+    }
+
+    private void shiftDown(int index) {
+        NodeArray<T> currentNode = this.getItemAt(index);
+        NodeArray<T> leftChild = this.getLeftChildOf(index);
+        NodeArray<T> rightChild = this.getRightChildOf(index);
+
+        if (currentNode.data.compareTo(leftChild.data) > 0) {
+            swap(currentNode, leftChild);
+        }
+
+        if (currentNode.data.compareTo(leftChild.data) > 0) {
+            swap(currentNode, leftChild);
+        }
+
+        this.shiftDown(rightChild.index);
+        this.shiftDown(leftChild.index);
+    }
+
+    private void swap(NodeArray<T> node1, NodeArray<T> node2) {
+        T tmp = node1.data;
+        node1.data = node2.data;
+        node2.data = tmp;
     }
 }
