18d80783eee629015fb4561630db1eb68772e4f6
Xrayya <nur.alkhiyar@gmail.com>
2022-11-30 17:34:22 +0700

    ILinkList testing

--- src/LinkedListApp.java
+++ src/LinkedListApp.java
@@ -1,25 +1,30 @@
 public class LinkedListApp {
 
     public static void main(String[] args) {
+        // data
+        Car car1 = new Car("Honda", "Jazz", 2004);
+        Car car2 = new Car("Toyota", "GR Yaris", 2022);
+        Car car3 = new Car("Mazda", "RX8", 2002);
+        Car car4 = new Car("Daihatsu", "Terios", 2015);
+        Car car5 = new Car("Daihatsu", "Luxio", 2009);
 
         // ILinkList
-        // ILinkList<Car> list = new LinkList<Car>();
-        //
-        // System.out.println("list isEmpty : " + list.isEmpty());
-        //
-        // list.addLast(new Car("Honda", "Jazz", 2004));
-        // list.addLast(new Car("Toyota", "GR Yaris", 2022));
-        // list.addLast(new Car("Mazda", "RX8", 2002));
-        //
-        // System.out.println("list isEmpty : " + list.isEmpty());
-        //
-        // System.out.println(list);
-        //
-        // list.addFirst(new Car("Suzuku", "APV", 2022));
-        //
-        // list.forwardTraverse();
-        // list.reverseTraverse();
-        //
-        // IPriorityQueue<Car> pQueue = new PriorityList<Car>(null);
+        ILinkList<Car> ill = new LinkList<>();
+
+        ill.addLast(car1);
+        ill.addLast(car2);
+        System.out.println(ill);
+
+        ill.addFirst(car3);
+        System.out.println(ill);
+
+        Node<Car> searchedCar = ill.find(new Car("Toyota", "GR Yaris", 2022));
+        System.out.println(searchedCar);
+
+        ill.addAfter(searchedCar, car4);
+        System.out.println(ill);
+
+        ill.addBefore(ill.find(new Car("Honda", "Jazz", 2004)), car5);
+        System.out.println(ill);
     }
 }
