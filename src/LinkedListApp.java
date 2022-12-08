34e6846d9e9eecb3ef7b60ee819e223c0a9a84d4
Xrayya <nur.alkhiyar@gmail.com>
2022-12-08 09:44:10 +0700

    fix all

--- src/LinkedListApp.java
+++ src/LinkedListApp.java
@@ -8,155 +8,158 @@ public class LinkedListApp {
         Car car4 = new Car("Daihatsu", "Terios", 2015);
         Car car5 = new Car("Daihatsu", "Luxio", 2009);
 
-        // // ILinkList
-        // ILinkList<Car> ill = new LinkList<>();
-        //
-        // System.out.println(ill.isEmpty());
-        //
-        // ill.addLast(car1);
-        // ill.addLast(car2);
-        // System.out.println(ill);
-        //
-        // System.out.println(ill.isEmpty());
-        //
-        // ill.addFirst(car3);
-        // System.out.println(ill);
-        //
-        // Node<Car> searchedCar = ill.find(new Car("Toyota", "GR Yaris", 2022));
-        // System.out.println(searchedCar);
-        //
-        // ill.addAfter(searchedCar, car4);
-        // System.out.println(ill);
-        //
-        // ill.addBefore(ill.find(new Car("Honda", "Jazz", 2004)), car5);
-        // System.out.println(ill);
-        //
-        // ill.forwardTraverse();
-        // ill.reverseTraverse();
-        //
-        // System.out.println(ill.removeFirst());
-        // System.out.println(ill.removeLast());
-        //
-        // System.out.println(ill);
-        //
-        // System.out.println(ill.removeAfter(car5));
-        //
-        // System.out.println(ill);
-        //
-        // System.out.println(ill.removeBefore(car2));
-        //
-        // System.out.println(ill);
-        //
-        // System.out.println(ill.remove(new Car("Toyota", "GR Yaris", 2022)));
-        //
-        // System.out.println(ill.isEmpty());
+        // ILinkList
+        ILinkList<Car> ill = new LinkList<>();
 
-        // // IStack
-        // IStack<Car> stack = new LinkList<>();
-        //
-        // stack.push(car1);
-        // stack.push(car2);
-        // stack.push(car3);
-        //
-        // System.out.println(stack);
-        //
-        // System.out.println(stack.pop());
-        // System.out.println(stack);
-        //
-        // System.out.println(stack.peek());
-        //
-        // System.out.println(stack.pop());
-        // System.out.println(stack.pop());
-        //
-        // System.out.println(stack);
-        // System.out.println(stack.isEmpty());
+        System.out.println(ill.isEmpty());
 
-        // // IQueue
-        // IQueue<Car> queue = new LinkList<>();
-        // queue.enqueue(car1);
-        // queue.enqueue(car2);
-        // queue.enqueue(car3);
-        //
-        // System.out.println(queue);
-        //
-        // System.out.println(queue.dequeue());
-        // System.out.println(queue);
-        //
-        // System.out.println(queue.dequeue());
-        // System.out.println(queue.dequeue());
-        //
-        // System.out.println(queue);
-        // System.out.println(queue.isEmpty());
+        ill.addLast(car1);
+        ill.addLast(car2);
+        System.out.println(ill);
 
-        // LinkList<Car> linkList = new LinkList<>();
-        //
-        // linkList.addLast(car1);
-        // linkList.addLast(car2);
-        // linkList.addLast(car3);
-        //
-        // System.out.println(linkList.size);
-        // System.out.println(linkList.removeLast());
-        // System.out.println(linkList.size);
-        //
-        // System.out.println(linkList);
-        // System.out.println(linkList.getFirst());
-        // System.out.println(linkList.getLast());
-        //
-        // linkList.addLast(car4);
-        // linkList.addLast(car5);
-        //
-        // System.out.println();
-        //
-        // System.out.println(linkList);
-        // for (Car car : linkList) {
-        // System.out.println(car);
-        // }
+        System.out.println(ill.isEmpty());
 
-        // // IPriorityList
-        // PriorityList<Car> pl = new PriorityList<>();
-        //
-        // pl.enqueue(car3);
-        // pl.enqueue(car1);
-        // pl.enqueue(car2);
-        // pl.enqueue(car4);
-        // pl.enqueue(car5);
-        //
-        // System.out.println(pl);
-        //
-        // pl.listAllElements();
-        //
-        // System.out.println();
-        //
-        // System.out.println(pl.getItemAt(3));
-        //
-        // System.out.println();
-        //
-        // System.out.println(pl.getParentOf(3));
-        // System.out.println(pl.getParentOf(4));
-        // System.out.println(pl.getParentOf(2));
-        //
-        // System.out.println();
-        //
-        // System.out.println(pl.getLeftChildOf(0));
-        // System.out.println(pl.getRightChildOf(1));
-        //
-        // System.out.println();
-        //
-        // for (Car car : pl) {
-        //     System.out.printf("%s - ", car);
-        // }
-        //
-        // System.out.println("\n");
-        //
-        // System.out.println(pl.peek());
-        // pl.dequeue();
-        //
-        // System.out.println(pl.dequeue());
-        // System.out.println(pl.dequeue());
-        //
-        // System.out.println(pl.peek());
-        // pl.dequeue();
-        //
-        // System.out.println(pl.dequeue());
+        ill.addFirst(car3);
+        System.out.println(ill);
+
+        Node<Car> searchedCar = ill.find(new Car("Toyota", "GR Yaris", 2022));
+        System.out.println(searchedCar);
+
+        ill.addAfter(searchedCar, car4);
+        System.out.println(ill);
+
+        System.out.println("dicari:");
+        System.out.println(ill.find(car3));
+
+        ill.addBefore(ill.find(new Car("Honda", "Jazz", 2004)), car5);
+        System.out.println(ill);
+
+        ill.forwardTraverse();
+        ill.reverseTraverse();
+
+        System.out.println(ill.removeFirst());
+        System.out.println(ill.removeLast());
+
+        System.out.println(ill);
+
+        System.out.println(ill.removeAfter(car5));
+
+        System.out.println(ill);
+
+        System.out.println(ill.removeBefore(car2));
+
+        System.out.println(ill);
+
+        System.out.println(ill.remove(new Car("Toyota", "GR Yaris", 2022)));
+
+        System.out.println(ill.isEmpty());
+
+        // IStack
+        IStack<Car> stack = new LinkList<>();
+
+        stack.push(car1);
+        stack.push(car2);
+        stack.push(car3);
+
+        System.out.println(stack);
+
+        System.out.println(stack.pop());
+        System.out.println(stack);
+
+        System.out.println(stack.peek());
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+
+        System.out.println(stack);
+        System.out.println(stack.isEmpty());
+
+        // IQueue
+        IQueue<Car> queue = new LinkList<>();
+        queue.enqueue(car1);
+        queue.enqueue(car2);
+        queue.enqueue(car3);
+
+        System.out.println(queue);
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue);
+
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+
+        System.out.println(queue);
+        System.out.println(queue.isEmpty());
+
+        LinkList<Car> linkList = new LinkList<>();
+
+        linkList.addLast(car1);
+        linkList.addLast(car2);
+        linkList.addLast(car3);
+
+        System.out.println(linkList.size);
+        System.out.println(linkList.removeLast());
+        System.out.println(linkList.size);
+
+        System.out.println(linkList);
+        System.out.println(linkList.getFirst());
+        System.out.println(linkList.getLast());
+
+        linkList.addLast(car4);
+        linkList.addLast(car5);
+
+        System.out.println();
+
+        System.out.println(linkList);
+        for (Car car : linkList) {
+            System.out.println(car);
+        }
+
+        // IPriorityList
+        PriorityList<Car> pl = new PriorityList<>();
+
+        pl.enqueue(car3);
+        pl.enqueue(car1);
+        pl.enqueue(car2);
+        pl.enqueue(car4);
+        pl.enqueue(car5);
+
+        System.out.println(pl);
+
+        pl.listAllElements();
+
+        System.out.println();
+
+        System.out.println(pl.getItemAt(3));
+
+        System.out.println();
+
+        System.out.println(pl.getParentOf(3));
+        System.out.println(pl.getParentOf(4));
+        System.out.println(pl.getParentOf(2));
+
+        System.out.println();
+
+        System.out.println(pl.getLeftChildOf(0));
+        System.out.println(pl.getRightChildOf(1));
+
+        System.out.println();
+
+        for (Car car : pl) {
+            System.out.printf("%s - ", car);
+        }
+
+        System.out.println("\n");
+
+        System.out.println(pl.peek());
+        pl.dequeue();
+
+        System.out.println(pl.dequeue());
+        System.out.println(pl.dequeue());
+
+        System.out.println(pl.peek());
+        pl.dequeue();
+
+        System.out.println(pl.dequeue());
     }
 }
